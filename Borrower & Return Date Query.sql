3c0175a86438a7b3e3a6732f2d05ccdf41a75f11
ShubhamPuri-SDET <Spuri4867@gmail.com>
2025-08-17 16:57:55 +0530

    Modified and arranged the code

--- Borrower & Return Date Query.sql
+++
@@ -1,5 +0,0 @@
--- Who borrowed which book and return date
-SELECT b.title, m.name, bb.borrow_date, bb.return_date
-FROM BorrowedBooks bb
-JOIN Books b ON bb.book_id = b.book_id
-JOIN Members m ON bb.member_id = m.member_id;
